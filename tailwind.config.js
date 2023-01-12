2c5a713b6fcccc0f5dcaf602c135529ddf9d4034
Jakub Bordáš <methalicity@gmail.com>
2023-01-12 21:30:52 +0100

    Added gradient background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}'],
-  theme: {
-    extend: {
-      screens: {
-        '2k': '2048px',
-        '2.5k': '2560px'
-      },
-      spacing: {
-        '24vw': '24vw',
-      },
-      maxWidth: {
-        'xxs': '16rem'
-      },
-      maxHeight: {
-        'xxs': '16rem'
-      }
-    },
-  },
-  plugins: [],
+	content: ['./**/*.{html,js}'],
+	theme: {
+		extend: {
+			screens: {
+				'2k': '2048px',
+				'2.5k': '2560px'
+			},
+			spacing: {
+				'24vw': '24vw',
+			},
+			maxWidth: {
+				'xxs': '16rem'
+			},
+			maxHeight: {
+				'xxs': '16rem'
+			},
+			backgroundImage: {
+				'gradient': 'url("/img/gradient.png")',
+			}
+		},
+	},
+	plugins: [],
 }
